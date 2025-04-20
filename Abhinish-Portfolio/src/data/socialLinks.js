0b5ea9ceb6c7921507fb433b97157bef2f292874
Abhinish Tiwari <abhinish990@gmail.com>
2025-04-20 14:37:06 +0530

    Update socialLinks.

--- Abhinish-Portfolio/src/data/socialLinks.js
+++ Abhinish-Portfolio/src/data/socialLinks.js
@@ -1,27 +1,27 @@
-import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope } from 'react-icons/fa';
+  import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope } from 'react-icons/fa';
 
 export const socialLinks = [
   {
     name: 'GitHub',
-    url: 'https://github.com/abhinish-raj',
+    url: 'https://github.com/abhinish7883',
     icon: FaGithub,
     hoverColor: 'hover:bg-gray-800'
   },
   {
-    name: 'LinkedIn',
-    url: 'https://linkedin.com/in/abhinish-raj',
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/abhinish-tiwari7883/",
     icon: FaLinkedin,
-    hoverColor: 'hover:bg-[#0077B5]'
+    hoverColor: "hover:bg-[#0077B5]",
   },
   {
-    name: 'Email',
-    url: 'mailto:abhinish990@gmail.com',
+    name: "Email",
+    url: "mailto:abhinish990@gmail.com",
     icon: FaEnvelope,
-    hoverColor: 'hover:bg-red-600'
+    hoverColor: "hover:bg-red-600",
   },
   {
-    name: 'Twitter',
-    url: 'https://twitter.com/abhinish_raj',
+    name: "Twitter",
+    url: "https://twitter.com/abhinish_raj",
     icon: FaTwitter,
     hoverColor: 'hover:bg-[#1DA1F2]'
   }
