90c26528d30e9096017f3c526443d3ed464bfe6f
Abhinish Tiwari <abhinish990@gmail.com>
2025-10-10 09:10:19 +0530

    Update resume link.

--- Abhinish-Portfolio/src/constants/resume.js
+++ Abhinish-Portfolio/src/constants/resume.js
@@ -1,4 +1,5 @@
 export const RESUME_CONFIG = {
-  fileName: 'Abhinish_Tiwari_Resume.pdf',
-  filePath: 'https://drive.google.com/file/d/1qBIdzyO_0CdXIwpguGjJAo4X8meAcis0/view',
+  fileName: "Abhinish_Tiwari_Resume.pdf",
+  filePath:
+    "https://drive.google.com/file/d/10_FFMGNnw_iz7-x8Z7Qs_5I2ayeFThID/view",
 };
